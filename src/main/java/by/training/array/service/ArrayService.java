c1fce84890afc5b4ef9239ec969784f217da07cb
Ihar Litvinko <i.litvinko.minsk@mail.ru>
2021-06-09 14:42:48 +0300

    Commit first

--- src/main/java/by/training/array/service/ArrayService.java
+++ src/main/java/by/training/array/service/ArrayService.java
@@ -4,6 +4,9 @@ import by.training.array.entity.ArrayMod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public final class ArrayService {
 
     static Logger logger = LogManager.getLogger();
@@ -11,38 +14,103 @@ public final class ArrayService {
     private ArrayService() {
     }
 
-
     //поиск min\max значения массива
-    public double minValueArray(ArrayMod array){
-        return 0.0;
+    public static double minValueArray(ArrayMod array){
+        double min;
+        if(array.getLength() > 0){
+            min = array.getArray()[0];
+            for (double value : array.getArray()) {
+                if (value < min){
+                    min = value;
+                }
+            }
+        }else {
+            min = 0.0;
+        }
+        logger.info("min value: " + min);
+        return min;
     }
 
-    public double maxValueArray(ArrayMod array){
-        return 0.0;
+    public static double maxValueArray(ArrayMod array){
+        double max;
+        if(array.getLength() > 0){
+            max = array.getArray()[0];
+            for (double value : array.getArray()) {
+                if (value > max){
+                    max = value;
+                }
+            }
+        }else {
+            max = 0.0;
+        }
+        logger.info("max value: " + max);
+        return max;
     }
 
     //определение среднего значения элементов массива
-    public double averageValueArray(ArrayMod array){
-        return 0.0;
+    public static double averageValueArray(ArrayMod array){
+        double average = 0.0;
+        if(array.getLength() > 0){
+            for (double value : array.getArray()) {
+                average += value;
+            }
+            average = average/array.getLength();
+        }
+        logger.info("average value: " + average);
+        return average;
     }
 
     //определение суммы элементов массива
-    public double sumAllValueArray(ArrayMod array){
-        return 0.0;
+    public static double sumAllValueArray(ArrayMod array){
+        double sum = 0.0;
+        if(array.getLength() > 0){
+            for (double value : array.getArray()) {
+                sum += value;
+            }
+        }
+        logger.info("sum of values: " + sum);
+        return sum;
     }
 
     //определение числа положительных\отрицательных элементов массива
-    public int countPositiveValueArray(ArrayMod array){
-        return 0;
+    public static int countPositiveValueArray(ArrayMod array){
+        int count = 0;
+        if(array.getLength() > 0){
+            for (double value : array.getArray()) {
+                if(value >= 0){
+                    count++;
+                }
+            }
+        }
+        logger.info("count of positive values: " + count);
+        return count;
     }
 
-    public int countNegativeValueArray(ArrayMod array){
-        return 0;
+    public static int countNegativeValueArray(ArrayMod array){
+        int count = 0;
+        if(array.getLength() > 0){
+            for (double value : array.getArray()) {
+                if(value < 0){
+                    count++;
+                }
+            }
+        }
+        logger.info("count of negative values: " + count);
+        return count;
     }
 
     //замену элементов массива по условию
-    public ArrayMod changeNegValueToZeroArray(ArrayMod array){
-        return null;
+    public static ArrayMod changeNegValueToZeroArray(ArrayMod array){
+        double[] changeArray = new double[array.getLength()];
+        for (int i = 0; i < array.getLength(); i++){
+            if(array.getArray()[i] >= 0){
+                changeArray[i] = array.getArray()[i];
+            }else {
+                changeArray[i] = 0;
+            }
+        }
+        logger.info("modified array: " + Arrays.toString(changeArray));
+        return new ArrayMod(changeArray);
     }
 
 }
--- src/test/java/by/training/array/test/TestArrayService.java
+++ src/test/java/by/training/array/test/TestArrayService.java
@@ -1,5 +1,93 @@
 package by.training.array.test;
 
+import by.training.array.entity.ArrayMod;
+import by.training.array.service.ArrayService;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class TestArrayService {
 
+    private ArrayMod basicArray;
+
+    @Before
+    public void initTest() {
+        basicArray = new ArrayMod();
+    }
+
+    @After
+    public void afterTest() {
+        basicArray = null;
+    }
+
+    @Test
+    public void minValueTest() {
+
+        basicArray.setArray(new double[]{3, 2, 8, 4, -5, 0});
+        double expected = -5.0;
+        double actual = ArrayService.minValueArray(basicArray);
+        Assert.assertEquals(actual, expected, 0.001);
+
+    }
+
+    @Test
+    public void maxValueTest() {
+
+        basicArray.setArray(new double[]{3, 2, 8, 4, -5, 0});
+        double expected = 8.0;
+        double actual = ArrayService.maxValueArray(basicArray);
+        Assert.assertEquals(actual, expected, 0.001);
+
+    }
+
+    @Test
+    public void averageValueTest() {
+
+        basicArray.setArray(new double[]{3, 2, 8, 4, -5, 0});
+        double expected = 2.0;
+        double actual = ArrayService.averageValueArray(basicArray);
+        Assert.assertEquals(actual, expected, 0.001);
+
+    }
+
+    @Test
+    public void sumValueTest() {
+
+        basicArray.setArray(new double[]{3, 2, 8, 4, -5, 0});
+        double expected = 12.0;
+        double actual = ArrayService.sumAllValueArray(basicArray);
+        Assert.assertEquals(actual, expected, 0.001);
+
+    }
+
+    @Test
+    public void countPositiveValueTest() {
+
+        basicArray.setArray(new double[]{3, 2, 8, 4, -5, 0});
+        int expected = 5;
+        int actual = ArrayService.countPositiveValueArray(basicArray);
+        Assert.assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void countNegativeValueTest() {
+
+        basicArray.setArray(new double[]{-2, -3, 8, -8, -5, 0});
+        int expected = 4;
+        int actual = ArrayService.countNegativeValueArray(basicArray);
+        Assert.assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void changeNegValueToZeroTest() {
+
+        basicArray.setArray(new double[]{-2, -3, 8, -8, -5, 0});
+        ArrayMod expected = new ArrayMod(new double[]{0, 0, 8, 0, 0, 0});
+        ArrayMod actual = ArrayService.changeNegValueToZeroArray(basicArray);
+        Assert.assertArrayEquals(expected.getArray(), actual.getArray(), 0.001);
+
+    }
 }
